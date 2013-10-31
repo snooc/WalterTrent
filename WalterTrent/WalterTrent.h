49c2685c3e62dd33916978254998eab72c005a71
Cody Coons <cody@codycoons.com>
2013-10-31 14:16:55 -0400

    Initial commit.

---
+++ WalterTrent/WalterTrent.h
@@ -0,0 +1,13 @@
+//
+//  WalterTrent.h
+//  WalterTrent
+//
+//  Created by Cody Coons on 10/31/13.
+//  Copyright (c) 2013 Cody Coons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WalterTrent : NSObject
+
+@end
---
+++ WalterTrent/WalterTrent.m
@@ -0,0 +1,13 @@
+//
+//  WalterTrent.m
+//  WalterTrent
+//
+//  Created by Cody Coons on 10/31/13.
+//  Copyright (c) 2013 Cody Coons. All rights reserved.
+//
+
+#import "WalterTrent.h"
+
+@implementation WalterTrent
+
+@end
